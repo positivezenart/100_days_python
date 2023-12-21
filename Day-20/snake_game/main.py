f151ac064d35f6ca354740efe47abfedbe5774d9
positivezenart <42809297+positivezenart@users.noreply.github.com>
2023-12-21 16:12:08 +0530

    snake game complete code

--- Day-20/snake_game/main.py
+++ Day-20/snake_game/main.py
@@ -1,10 +1,14 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import *
+from food import *
+from score_board import *
 
 screen = Screen()
 
 snake = Snake()
+food = Food()
+score = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -17,5 +21,25 @@ while is_game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    
+    #detect collision with food
+    if snake.head.distance(food) < 15:
+       food.refresh()
+       snake.extend()
+       score.increaseScore()
+       
+     #detect collision with   
+    if snake.head.xcor() >285 or snake.head.xcor() < -285 or snake.head.ycor() > 285 or snake.head.ycor() < -285:
+        score.Gameover()
+        is_game_on = False
+        
+    #detec collision with 
+    for segments in snake.seg_ments:
+        if segments == snake.head:
+            pass
+        elif snake.head.distance(segments) < 10:
+            score.Gameover()
+            is_game_on = False
+       
 
 screen.exitonclick()
